de1726b14a3c86fde8558cc4466445ca50da2ddc
unknown <flavian77dumitrache@gmail.com>
2024-04-15 14:01:09 +0300

    getting followers ids

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ dotenv.config({
 
 puppeteer.use(StealthPlugin());
 
-const WAIT_TIME = 800;
+const WAIT_TIME = 1000;
+
+const PROXY_USERNAME = process.env.PROXY_USERNAME;
+const PROXY_PASSWORD = process.env.PROXY_PASSWORD;
 
 async function run() {
   const browser = await puppeteer.launch({
     headless: false,
-    // defaultViewport: null,
     args: [
-      //   "--start-maximized",
-      "--proxy-server=http=brd.superproxy.io:22225",
-      "--proxy-auth=brd-customer-hl_d8fb0978-zone-isp_proxy1:8kpzm52uuy6p",
+      // "--proxy-server=https://ro.smartproxy.com:13001",
     ],
   });
   const page = await browser.newPage();
+
+  // Authenticating the proxy server
+  // await page.authenticate({ username: PROXY_USERNAME, password: PROXY_PASSWORD });
 
   await page.goto("https://www.instagram.com/");
 
@@ -31,13 +34,10 @@ async function run() {
   );
   await rejectAllButton.click();
 
-  //   const usernameInput = await page.waitForSelector("aria/Phone number, username, or email");
-  //   await usernameInput.fill("tbflavian");
-
   // Fill in login form
   await page.locator("aria/Phone number, username, or email").fill(process.env.INSTAGRAM_USERNAME);
   await page.locator("aria/Password").fill(process.env.INSTAGRAM_PASSWORD);
-  await new Promise((r) => setTimeout(r, WAIT_TIME));
+  await new Promise((r) => setTimeout(r, 1500));
 
   // Log in
   const loginButton = await page.waitForSelector("text/Log in");
@@ -65,25 +65,27 @@ async function run() {
   await new Promise((r) => setTimeout(r, WAIT_TIME));
   await followersListButton.click();
 
-  //   /html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div/div/div/div/div/a/div/div
   await new Promise((r) => setTimeout(r, WAIT_TIME));
   const followersContainerPath = "xpath///div[@class='_aano']";
 
-  // Getting all followers
+  // Scrolling to the bottom, in order to have all usernames in the DOM
   console.log("Getting followers list...");
   for (let i = 1; i <= Math.floor(Number(followersNum) / 5); i++) {
     await page.waitForSelector(followersContainerPath);
     await page.locator(followersContainerPath).scroll({
       scrollTop: 300 * i,
     });
-    // await lastFollowerFromBatch.scrollIntoView();
-    await new Promise((r) => setTimeout(r, WAIT_TIME));
-    // const href = await lastFollowerFromBatch.evaluate((el) => el.href);
-    // console.log(href);
+
+    await new Promise((r) => setTimeout(r, 800));
   }
 
+  // Getting the usernames and coresponding user ids
+
+  // Opening a new page in which the GET requests will take place
+  const dataPage = await browser.newPage();
   const followersList = [];
   for (let i = 1; i <= Number(followersNum); i++) {
+    // Getting the username
     const follower = await page.waitForSelector(
       `${followersContainerPath}/div[1]/div/div[${i}]/div/div/div/div[2]/div/div/div/div/div/a`
     );
@@ -91,24 +93,38 @@ async function run() {
     const username = href.split("/")[3];
 
     // Getting user id
+    // GET requests in the browser (log in is required)
     const url = `https://www.instagram.com/web/search/topsearch/?query=${username}`;
 
-    let userId = "";
-    // TODO: get the user id of each follower
-    // GET requests in the browser (log in is required)
+    const userDoc = await dataPage.goto(url, { waitUntil: "domcontentloaded" });
+    const responseBody = await userDoc.text();
+    let userData = null;
+    try {
+      userData = JSON.parse(responseBody);
+    } catch (err) {
+      console.log(err);
+    }
+    const userId = userData?.users[0]?.user?.pk ?? "";
 
     followersList.push({ username, userId });
+
+    // await new Promise((r) => setTimeout(r, 500));
   }
 
+  // Writing to file
   const jsonString = JSON.stringify(followersList, null, 2);
 
   fs.writeFile("./followers.json", jsonString, (err) => {
     if (err) {
       console.error("Error writing JSON to file:", err);
     } else {
-      console.log("JSON data has been saved to output.json");
+      console.log("Followers list has been saved to followers.json");
     }
   });
+
+  await browser.close();
 }
 
 run();
+
+/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/div/div/div[680]
